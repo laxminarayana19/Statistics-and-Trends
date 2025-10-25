654dbcbcef26e565ea3bdfa982e857df52eb9c0a
laxminarayana19 <laxminarayana77990@gmail.com>
2025-10-25 18:49:42 +0100

    Update statistics_and_trends.py

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -3,7 +3,7 @@ This is the template file for the statistics and trends assignment.
 You will be expected to complete all the sections and
 make this a fully working, documented file.
 You should NOT change any function, file or variable names,
- if they are given to you here.
+if they are given to you here.
 Make use of the functions presented in the lectures
 and ensure your code is PEP-8 compliant, including docstrings.
 """
@@ -14,62 +14,93 @@ import pandas as pd
 import scipy.stats as ss
 import seaborn as sns
 
-
 def plot_relational_plot(df):
+    """
+    Plots a relational scatterplot between total_bill and tip.
+    """
     fig, ax = plt.subplots()
+    sns.scatterplot(x='total_bill', y='tip', data=df, ax=ax)
+    ax.set_title('Total Bill vs Tip (Relational Plot)')
     plt.savefig('relational_plot.png')
+    plt.close(fig)
     return
-
 
 def plot_categorical_plot(df):
+    """
+    Plots a categorical boxplot for tip amount by day.
+    """
     fig, ax = plt.subplots()
+    sns.boxplot(x='day', y='tip', data=df, ax=ax)
+    ax.set_title('Tip Amount by Day (Categorical Plot)')
     plt.savefig('categorical_plot.png')
+    plt.close(fig)
     return
-
 
 def plot_statistical_plot(df):
+    """
+    Plots a histogram for total_bill.
+    """
     fig, ax = plt.subplots()
+    sns.histplot(df['total_bill'], bins=20, kde=True, ax=ax)
+    ax.set_title('Distribution of Total Bill (Statistical Plot)')
     plt.savefig('statistical_plot.png')
+    plt.close(fig)
     return
 
-
 def statistical_analysis(df, col: str):
-    mean =
-    stddev =
-    skew =
-    excess_kurtosis =
+    """
+    Computes mean, standard deviation, skewness, and excess kurtosis for the given column.
+    """
+    mean = df[col].mean()
+    stddev = df[col].std()
+    skew = ss.skew(df[col], nan_policy='omit')
+    excess_kurtosis = ss.kurtosis(df[col], nan_policy='omit')
     return mean, stddev, skew, excess_kurtosis
 
-
 def preprocessing(df):
-    # You should preprocess your data in this function and
-    # make use of quick features such as 'describe', 'head/tail' and 'corr'.
+    """Preprocesses the dataset, provides quick stats and returns the cleaned dataframe."""
+    print(df.head())
+    print(df.describe())
+    print(df.select_dtypes(include=[np.number]).corr())
+    # No complex cleaning needed for tips dataset, but placeholder for future.
     return df
 
-
 def writing(moments, col):
+    """
+    Prints the calculated statistics and interprets skew/kurtosis.
+    """
     print(f'For the attribute {col}:')
     print(f'Mean = {moments[0]:.2f}, '
           f'Standard Deviation = {moments[1]:.2f}, '
           f'Skewness = {moments[2]:.2f}, and '
           f'Excess Kurtosis = {moments[3]:.2f}.')
-    # Delete the following options as appropriate for your data.
-    # Not skewed and mesokurtic can be defined with asymmetries <-2 or >2.
-    print('The data was right/left/not skewed and platy/meso/leptokurtic.')
-    return
 
+    if abs(moments[2]) < 0.5:
+        skewness_type = 'not skewed'
+    elif moments[2] > 0:
+        skewness_type = 'right skewed'
+    else:
+        skewness_type = 'left skewed'
+
+    if moments[3] > 0.5:
+        kurtosis_type = 'leptokurtic'
+    elif moments[3] < -0.5:
+        kurtosis_type = 'platykurtic'
+    else:
+        kurtosis_type = 'mesokurtic'
+    return
 
 def main():
     df = pd.read_csv('data.csv')
     df = preprocessing(df)
-    col = '<your chosen column for analysis>'
+    col = 'total_bill'  # Example analysis column from tips dataset
     plot_relational_plot(df)
     plot_statistical_plot(df)
     plot_categorical_plot(df)
     moments = statistical_analysis(df, col)
     writing(moments, col)
     return
-
+plt.show()
 
 if __name__ == '__main__':
     main()
